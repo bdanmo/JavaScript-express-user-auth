53bbda90df03a9f789d595f00341fff1e6ef3be3
Brendan Moran <b.daniel.moran@gmail.com>
2017-01-21 20:16:23 -0500

    setup mongoose model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require(mongoose),
+
+UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    favoriteBook: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}); //end of Schema constructor
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
